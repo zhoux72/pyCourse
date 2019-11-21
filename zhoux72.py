8fea5e8c7167bf4618f002668c38e8c72bdd0982
zhoux72 <53201625+zhoux72@users.noreply.github.com>
2019-11-21 10:08:42 +0800

    Add files via upload

---
+++ zhoux72.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import re 
+import requests  
+from pyquery import PyQuery as pq
+def getHtml(url, code="utf-8"):  
+    try:  
+        r =requests.get(url)  
+        r.raise_for_status()  
+        r.encoding = code  
+        return r.text  
+    except:  
+        return ""   
+def main():  
+    print("start")
+    url='http://www.baidu.com/s?wd=zhoux72'  
+    doc= pq(getHtml(url))
+    mlist = doc('#content_left h3.t a').items()
+    i=0
+    for li in mlist :
+        i=i+1
+        print('标题：'+li.text())
+        print('链接：'+li.attr('href'))
+    print(i)
+    print("end")  
+main()
